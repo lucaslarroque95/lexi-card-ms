695e6dc840befa1d44fcbc60570a54a154c65006
lucas.larroque <lucasdlm95@gmail.com>
2026-09-01 02:16:56 +0000

    v1.0.1

--- src/main.py
+++ src/main.py
@@ -45,4 +45,4 @@ def on_startup():
     create_db_and_tables()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, root_path=os.getenv("ROOT_PATH", ""))
